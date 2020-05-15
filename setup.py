9e6469da9cee2e1282434e468a939198addc9ea7
Jacob Moorman <jdmoorman@math.ucla.edu>
2020-05-15 14:22:00 -0700

    Add tests with pytest and tox. Add GitHub Actions.

--- setup.py
+++ setup.py
@@ -9,25 +9,25 @@ with open("README.md") as readme_file:
     readme = readme_file.read()
 
 test_requirements = [
-    
+    "pytest", "pytest-cov",
 ]
 
 docs_requirements = [
-    
+
 ]
 
 setup_requirements = [
-    
+
 ]
 
 dev_requirements = [
     *test_requirements,
     *docs_requirements,
     *setup_requirements,
-    
+
     "bump2version>=1.0.0",
     "ipython>=7.5.0",
-    
+    "tox>=3.5.2",
     "twine>=1.13.0",
     "wheel>=0.33.1",
 ]
---
+++ tests/test_function.py
@@ -0,0 +1,5 @@
+from python_boilerplate import Example
+
+def test_example():
+    e = Example()
+    assert e.get_value() == 10
